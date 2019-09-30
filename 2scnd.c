fbd4983fd9beb3df0791ab32fc647bd3d6a4026d
MMalevany <55573348+MMalevany@users.noreply.github.com>
2019-09-30 21:22:51 +0300

    full code
    ez pz lmn sqzzz

--- 2scnd.c
+++ 2scnd.c
@@ -2,33 +2,28 @@
 #include <stdio.h>
 #include <math.h>
 
-float s,x,y,z,temp,tmp,flex,a,b,c;
-
-float e=2.718281828;
-
 int main(int argc, char const *argv[])
 {
-    //scanf("%f%f%f", &x,&y,&z);
-    x=-4.5;
-    y=0.75*powf(10,-4);
-    z=-0.845*pow(10,2);
+	float a,b,c,z,x,y,s;
+	float e=2.71828;
 
-    tmp=x-y;
-    flex=2;
+	/*
+	x=-4.5;
+	y=0.75*pow(10,-4);
+	z=-0.845*pow(10,2);
+	*/
 
-    a=powf(tmp,flex);
-    a+=9;
-    a=pow(a,1./3);
+	scanf("%f%f%f", &x, &y, &z);
 
-    b=powf(x,2)+pow(y,2)+2;
+	a=pow((9+pow((x-y),2)),1.0/3);
+	b=pow(x,2)+pow(y,2)+2;
+	c=pow(e,fabs(x-y))*pow(tan(z),3);
 
-    c=pow(e,abs(x-y))*pow(tan(z),3);
+	s=a/b-c;
 
-    s=a/b-c;
+	printf("Result: ");
+	printf("%f", s);
+	printf("\n");
 
-    printf("%f", s);
-
-    return 0;
+	return 0;
 }
-
-
